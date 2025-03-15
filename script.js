b2dc2070dfa06088b41cfc665e94eab1426669d6
Brandon Powell <96079320+BrdnPwll70@users.noreply.github.com>
2025-03-15 14:20:01 -0500

    finished CSS and Script.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.social-linksa');
 
+    links.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.style.backgroundColor = '#555'; 
+        });
+
+        link.addEventListener('mouseout', () => {
+            link.style.backgroundColor = '#333';
+        });
+    });
+});
